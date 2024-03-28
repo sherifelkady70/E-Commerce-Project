f91cd53a29ee7ee269827a39673f85e4b25e9fed
Nadia Seleem <nadiaseleem.route@gmail.com>
2024-03-28 21:46:46 +0200

    + added xml designs

---
+++ app/build.gradle.kts
@@ -0,0 +1,68 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.kotlin.parcelize)
+
+}
+
+android {
+    namespace = "com.example.routee_commerce"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.routee_commerce"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    //navigation component
+    implementation(libs.androidx.navigation.fragment)
+    implementation(libs.androidx.navigation.ui)
+
+    //shimmer effect
+    implementation(libs.shimmer.effect)
+    //rounded image
+    implementation(libs.roundedimage)
+
+    //picasso
+    implementation(libs.picasso)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+
+}
---
+++ app/src/main/java/com/example/routee_commerce/utils/BindingAdapters.kt
@@ -0,0 +1,32 @@
+package com.example.routee_commerce.utils
+
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.example.routee_commerce.R
+import com.squareup.picasso.Picasso
+
+class BindingAdapters {
+    companion object {
+        @BindingAdapter("app:urlCircular")
+        @JvmStatic
+        fun bindImageCircular(imageView: ImageView, url: String?) {
+            Picasso.get()
+                .load(url)
+                .placeholder(R.drawable.ic_category_placeholder)
+                .transform(TransformCircular())
+                .into(imageView)
+        }
+
+        @BindingAdapter("app:url")
+        @JvmStatic
+        fun bindImage(imageView: ImageView, url: String?) {
+            Picasso.get()
+                .load(url)
+                .centerCrop()
+                .fit()
+                .into(imageView)
+        }
+
+    }
+}
