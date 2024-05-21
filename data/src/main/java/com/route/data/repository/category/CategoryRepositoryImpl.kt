657ccd7ed33749891947d3c37d47af7f0e680a9a
sherif Abdelrhman <sellkady038@gmail.com>
2024-05-21 03:16:08 +0300

    handling repositories in data and domain .

--- data/src/main/java/com/route/data/repository/category/CategoryRepositoryImpl.kt
+++ data/src/main/java/com/route/data/repository/category/CategoryRepositoryImpl.kt
@@ -9,7 +9,7 @@ import javax.inject.Inject
 class CategoryRepositoryImpl @Inject constructor(
     private val categoryOnlineDatasource: CategoryOnlineDatasource
 ) : CategoriesRepository {
-    override suspend fun getAllCategories(): Flow<List<Category>> {
+    override suspend fun getAllCategories(): Flow<List<Category>?> {
         return categoryOnlineDatasource.getAllCategories()
     }
 }
