9861eeb26ca5410d2c3fda81e76531845a45eaca
sherif Abdelrhman <sellkady038@gmail.com>
2024-05-23 10:58:15 +0300

    handle shimmer in XML and home fragment.

--- app/src/main/java/com/example/routee_commerce/ui/home/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/example/routee_commerce/ui/home/fragments/home/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
@@ -86,8 +87,9 @@ class HomeFragment : BaseFragment<HomeFragmentViewModel,FragmentHomeBinding>() {
 
     }
 
-    fun myObserveLiveData() {
+    private fun myObserveLiveData() {
         viewModel.categoriesList.observe(viewLifecycleOwner){
+            dataBinding.categoriesShimmerViewContainer.hideShimmer()
             if (it != null) {
                 categoriesAdapter.bindCategories(it)
             }
