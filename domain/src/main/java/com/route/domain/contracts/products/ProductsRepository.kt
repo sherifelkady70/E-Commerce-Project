87b20c9ad7e52b1a812877478a6be7dafbce221e
sherif Abdelrhman <sellkady038@gmail.com>
2024-05-28 18:19:22 +0300

    Products Repository in domain .

---
+++ domain/src/main/java/com/route/domain/contracts/products/ProductsRepository.kt
@@ -0,0 +1,19 @@
+package com.route.domain.contracts.products
+
+import com.route.domain.common.Resource
+import com.route.domain.models.Products
+import kotlinx.coroutines.flow.Flow
+
+interface ProductsRepository {
+    suspend fun getProducts(
+        categoryId : String?,
+        sortedBy : SortedBy?,
+        brandId : String?
+    ) : Flow<Resource<List<Products>?>>
+}
+
+enum class SortedBy (val value : String){
+    PRICE_DESC("-price"),
+    PRICE_ASC("price"),
+    MOST_SELLING("-sold")
+}
