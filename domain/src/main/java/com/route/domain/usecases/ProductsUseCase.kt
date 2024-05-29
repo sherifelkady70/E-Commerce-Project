63479ccbf74d347cf7eac29b04ae992a9efcd46f
sherif Abdelrhman <sellkady038@gmail.com>
2024-05-29 13:58:31 +0300

    Products UseCase .

--- domain/src/main/java/com/route/domain/usecases/ProductsUseCase.kt
+++ domain/src/main/java/com/route/domain/usecases/ProductsUseCase.kt
@@ -1,10 +1,15 @@
 package com.route.domain.usecases
 
+import com.route.domain.common.Resource
 import com.route.domain.contracts.products.ProductsRepository
+import com.route.domain.models.Products
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class ProductsUseCase @Inject constructor(
     private val ProductsRepository : ProductsRepository
 ) {
-
+    suspend fun invoke() : Flow<Resource<List<Products>?>>{
+        return ProductsRepository.getProducts()
+    }
 }
