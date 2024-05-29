34018e57688aa7825208c5c231587b1e18cdf9a1
sherif Abdelrhman <sellkady038@gmail.com>
2024-05-29 13:29:38 +0300

    Network Call in Web Services .

--- data/src/main/java/com/route/data/api/WebServices.kt
+++ data/src/main/java/com/route/data/api/WebServices.kt
@@ -1,6 +1,7 @@
 package com.route.data.api
 
 import com.route.data.api.model.CategoryDTO
+import com.route.data.api.model.ProductsDTO
 import com.route.data.api.model.Response
 import kotlinx.coroutines.flow.Flow
 import retrofit2.http.GET
@@ -8,4 +9,7 @@ import retrofit2.http.GET
 interface WebServices {
     @GET("/api/v1/categories")
     suspend fun getCategories() : Response<List<CategoryDTO?>?>
+
+    @GET("/api/v1/products")
+    suspend fun getProducts() : Response<List<ProductsDTO?>?>
 }
