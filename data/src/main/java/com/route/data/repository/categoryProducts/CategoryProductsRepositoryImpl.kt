5f11058df506f5b6348200df4afdfd70882b8fac
sherif Abdelrhman <sellkady038@gmail.com>
2024-05-30 07:58:45 +0300

    make DI object for datasource  .

---
+++ data/src/main/java/com/route/data/repository/categoryProducts/CategoryProductsRepositoryImpl.kt
@@ -0,0 +1,17 @@
+package com.route.data.repository.categoryProducts
+
+import com.route.data.contracts.categoryproducts.CategoryProductsOnlineDatasource
+import com.route.domain.common.Resource
+import com.route.domain.contracts.categoryproducts.CategoryProductsRepository
+import com.route.domain.models.Products
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+abstract class CategoryProductsRepositoryImpl  @Inject constructor(
+    private val categoryProductsOnlineDatasource: CategoryProductsOnlineDatasource
+): CategoryProductsRepository
+{
+    override suspend fun getCategoryProducts(): Flow<Resource<List<Products>?>> {
+        TODO("Not yet implemented")
+    }
+}
