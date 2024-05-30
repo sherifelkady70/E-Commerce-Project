f050832ace07ea4f1ee4b2afaff74d7da5763ef5
sherif Abdelrhman <sellkady038@gmail.com>
2024-05-30 08:05:25 +0300

    finish category products use case  .

--- domain/src/main/java/com/route/domain/usecases/CategoryProductsUseCase.kt
+++ domain/src/main/java/com/route/domain/usecases/CategoryProductsUseCase.kt
@@ -1,4 +1,15 @@
 package com.route.domain.usecases
 
-class CategoryProductsUseCase {
+import com.route.domain.common.Resource
+import com.route.domain.contracts.categoryproducts.CategoryProductsRepository
+import com.route.domain.models.Products
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class CategoryProductsUseCase @Inject constructor(
+    private val categoryProductsRepository: CategoryProductsRepository
+){
+    suspend fun getCategoryProducts() : Flow<Resource<List<Products>?>> {
+        return categoryProductsRepository.getCategoryProducts()
+    }
 }
