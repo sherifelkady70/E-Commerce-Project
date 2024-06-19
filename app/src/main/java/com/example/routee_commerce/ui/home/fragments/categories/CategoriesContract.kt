019a18339b0f014afa62e4b96b0074206f9d9bde
sherif Abdelrhman <sellkady038@gmail.com>
2024-06-19 18:41:53 +0300

    create categories contract for categories in sub categories fragment.

--- app/src/main/java/com/example/routee_commerce/ui/home/fragments/categories/CategoriesContract.kt
+++ app/src/main/java/com/example/routee_commerce/ui/home/fragments/categories/CategoriesContract.kt
@@ -1,10 +1,14 @@
 package com.example.routee_commerce.ui.home.fragments.categories
 
-class CategoriesContract {
+import androidx.lifecycle.LiveData
+import kotlinx.coroutines.flow.StateFlow
+ class CategoriesContract {
 
     interface CategoriesViewModel {
         fun doAction(action : Action)
 
+        val event : LiveData<Event>
+        val state : StateFlow<State>
     }
 
     sealed class Action {
--- app/src/main/java/com/example/routee_commerce/ui/home/fragments/categories/CategoriesFragmentViewModel.kt
+++ app/src/main/java/com/example/routee_commerce/ui/home/fragments/categories/CategoriesFragmentViewModel.kt
@@ -1,13 +1,31 @@
 package com.example.routee_commerce.ui.home.fragments.categories
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.route.domain.usecases.CategoryUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class CategoriesFragmentViewModel @Inject constructor(
-    val categoriesUseCase : CategoryUseCase
-) : ViewModel() {
+    val categoriesUseCase : CategoryUseCase,
+    override val event: LiveData<CategoriesContract.Event>,
+    override val state: StateFlow<CategoriesContract.State>
+) : ViewModel() , CategoriesContract.CategoriesViewModel {
+    override fun doAction(action: CategoriesContract.Action) {
+        when (action){
+            CategoriesContract.Action.InitPage -> {
+                initPage()
+            }
+        }
+    }
 
+
+    private fun initPage() {
+        getCategories()
+    }
+    private fun getCategories() {
+
+    }
 }
