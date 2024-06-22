0f92140e3c99a9311c3cc7fc55b9af2992ccb61d
sherif Abdelrhman <sellkady038@gmail.com>
2024-06-22 19:11:59 +0300

    create subcategory object in repository module.

--- data/src/main/java/com/route/data/repository/RepositoryModule.kt
+++ data/src/main/java/com/route/data/repository/RepositoryModule.kt
@@ -3,9 +3,11 @@ package com.route.data.repository
 import com.route.data.repository.category.CategoryRepositoryImpl
 import com.route.data.repository.categoryProducts.CategoryProductsRepositoryImpl
 import com.route.data.repository.products.ProductsRepositoryImpl
+import com.route.data.repository.subcategory.SubCategoryRepositoryImpl
 import com.route.domain.contracts.category.CategoriesRepository
 import com.route.domain.contracts.categoryproducts.CategoryProductsRepository
 import com.route.domain.contracts.products.ProductsRepository
+import com.route.domain.contracts.subcategory.SubCategoryRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -26,4 +28,8 @@ abstract class RepositoryModule {
     @Binds
     abstract fun provideCategoryProductsRepository(impl : CategoryProductsRepositoryImpl)
     : CategoryProductsRepository
+
+    @Binds
+    abstract fun provideSubCategoryRepository(impl : SubCategoryRepositoryImpl)
+    : SubCategoryRepository
 }
