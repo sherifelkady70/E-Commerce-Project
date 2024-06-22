1bdfb9f83498eccb3d09ecff15d2ed2041b99fa2
sherif Abdelrhman <sellkady038@gmail.com>
2024-06-22 19:00:52 +0300

    create subcategory datasource impl object in datasource module.

--- data/src/main/java/com/route/data/datasource/OnlineDatasourceModule.kt
+++ data/src/main/java/com/route/data/datasource/OnlineDatasourceModule.kt
@@ -3,9 +3,11 @@ package com.route.data.datasource
 import com.route.data.contracts.category.CategoryOnlineDatasource
 import com.route.data.contracts.categoryproducts.CategoryProductsOnlineDatasource
 import com.route.data.contracts.products.ProductOnlineDataSource
+import com.route.data.contracts.subcategory.SubCategoryDataSource
 import com.route.data.datasource.category.CategoryOnlineDatasourceImpl
 import com.route.data.datasource.categoryproducts.CategoryProductsOnlineDatasourceImpl
 import com.route.data.datasource.products.ProductsOnlineDatasourceImpl
+import com.route.data.datasource.subcategory.SubCategoryDataSourceImpl
 import com.route.data.repository.products.ProductsRepositoryImpl
 import com.route.domain.contracts.products.ProductsRepository
 import dagger.Binds
@@ -28,4 +30,8 @@ abstract class OnlineDatasourceModule {
     @Binds
     abstract fun provideCategoryProductsDatasource(impl : CategoryProductsOnlineDatasourceImpl)
     : CategoryProductsOnlineDatasource
+
+    @Binds
+    abstract fun provideSunCategoryDatasource(impl : SubCategoryDataSourceImpl)
+    : SubCategoryDataSource
 }
