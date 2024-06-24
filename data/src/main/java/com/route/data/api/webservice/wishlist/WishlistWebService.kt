c388b9b018bfdf9237c5a3487a5f8ebc439a68d2
sherif Abdelrhman <sellkady038@gmail.com>
2024-06-24 19:03:41 +0300

    create get log user for wishlist fun in wishlist webservice in data .

--- data/src/main/java/com/route/data/api/webservice/wishlist/WishlistWebService.kt
+++ data/src/main/java/com/route/data/api/webservice/wishlist/WishlistWebService.kt
@@ -1,8 +1,10 @@
 package com.route.data.api.webservice.wishlist
 
+import com.route.data.api.model.wishlist.WishlistDTO
 import com.route.data.api.model.wishlist.WishlistResponse
 import retrofit2.http.DELETE
 import retrofit2.http.Field
+import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
 import retrofit2.http.Path
@@ -20,4 +22,9 @@ interface WishlistWebService {
         @Header("token") token : String,
         @Path("productId") productId : String
     ): WishlistResponse<List<String>?>
+
+    @GET("/api/v1/wishlist")
+    suspend fun getLoggedUserWishlist(
+        @Header("token") token : String
+    ):WishlistResponse<List<WishlistDTO>?>
 }
