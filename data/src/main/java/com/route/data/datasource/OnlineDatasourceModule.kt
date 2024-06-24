7f2330208704d663238e7756e51b7b8477ec7ded
sherif Abdelrhman <sellkady038@gmail.com>
2024-06-24 19:16:10 +0300

    create wishlist datasource object in module in data .

--- data/src/main/java/com/route/data/datasource/OnlineDatasourceModule.kt
+++ data/src/main/java/com/route/data/datasource/OnlineDatasourceModule.kt
@@ -4,10 +4,12 @@ import com.route.data.contracts.category.CategoryOnlineDatasource
 import com.route.data.contracts.categoryproducts.CategoryProductsOnlineDatasource
 import com.route.data.contracts.products.ProductOnlineDataSource
 import com.route.data.contracts.subcategory.SubCategoryDataSource
+import com.route.data.contracts.wishlist.WishlistDataSource
 import com.route.data.datasource.category.CategoryOnlineDatasourceImpl
 import com.route.data.datasource.categoryproducts.CategoryProductsOnlineDatasourceImpl
 import com.route.data.datasource.products.ProductsOnlineDatasourceImpl
 import com.route.data.datasource.subcategory.SubCategoryDataSourceImpl
+import com.route.data.datasource.wishlist.WishlistDataSourceImpl
 import com.route.data.repository.products.ProductsRepositoryImpl
 import com.route.domain.contracts.products.ProductsRepository
 import dagger.Binds
@@ -34,4 +36,8 @@ abstract class OnlineDatasourceModule {
     @Binds
     abstract fun provideSunCategoryDatasource(impl : SubCategoryDataSourceImpl)
     : SubCategoryDataSource
+
+    @Binds
+    abstract fun provideWishlistDatasource(impl: WishlistDataSourceImpl)
+    : WishlistDataSource
 }
