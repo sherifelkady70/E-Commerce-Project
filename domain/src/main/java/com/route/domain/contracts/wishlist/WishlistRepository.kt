6fb1f3ed17f31b2d69629ffdabbb018d126c8356
sherif Abdelrhman <sellkady038@gmail.com>
2024-06-24 19:32:46 +0300

    create wishlist repository interface in domain .

--- domain/src/main/java/com/route/domain/contracts/wishlist/WishlistRepository.kt
+++ domain/src/main/java/com/route/domain/contracts/wishlist/WishlistRepository.kt
@@ -1,10 +1,22 @@
 package com.route.domain.contracts.wishlist
 
 import com.route.domain.common.Resource
+import com.route.domain.models.WishlistItem
+import com.route.domain.models.WishlistResponse
+import kotlinx.coroutines.flow.Flow
 
 interface WishlistRepository {
     suspend fun addProductToWishlist(
         token : String,
         productId : String
-    ) : Flow<Resource<Wishlis>>
+    ) : Flow<Resource<WishlistResponse<List<String>?>>>
+
+    suspend fun deleteProductFromWishlist(
+        token: String,
+        productId: String
+    ) : Flow<Resource<WishlistResponse<List<String>?>>>
+
+    suspend fun getLoggedUserWishlist(
+        token: String
+    ) : Flow<Resource<List<WishlistItem>?>>
 }
