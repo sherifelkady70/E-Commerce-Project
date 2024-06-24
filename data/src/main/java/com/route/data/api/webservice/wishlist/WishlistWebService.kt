def82c63eb08084c90a2c991ab970cee2b6f917b
sherif Abdelrhman <sellkady038@gmail.com>
2024-06-24 19:01:38 +0300

    create delete product fun in wishlist webservice in data .

--- data/src/main/java/com/route/data/api/webservice/wishlist/WishlistWebService.kt
+++ data/src/main/java/com/route/data/api/webservice/wishlist/WishlistWebService.kt
@@ -1,9 +1,11 @@
 package com.route.data.api.webservice.wishlist
 
 import com.route.data.api.model.wishlist.WishlistResponse
+import retrofit2.http.DELETE
 import retrofit2.http.Field
 import retrofit2.http.Header
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface WishlistWebService {
 
@@ -13,5 +15,9 @@ interface WishlistWebService {
         @Field("productId") productId : String
     ):WishlistResponse<List<String>?>
 
-
+    @DELETE("/api/v1/wishlist/{productId}")
+    suspend fun deleteProductFromWishlist(
+        @Header("token") token : String,
+        @Path("productId") productId : String
+    ): WishlistResponse<List<String>?>
 }
