26037c2d1fa2e064df23fb0ae8df75c5f6e93c39
sherif Abdelrhman <sellkady038@gmail.com>
2024-07-09 07:03:30 +0300

    finish sealed class (states and events).

--- app/src/main/java/com/example/routee_commerce/ui/userAuthentication/fragments/login/LoginContract.kt
+++ app/src/main/java/com/example/routee_commerce/ui/userAuthentication/fragments/login/LoginContract.kt
@@ -1,6 +1,8 @@
 package com.example.routee_commerce.ui.userAuthentication.fragments.login
 
 import androidx.lifecycle.LiveData
+import com.example.routee_commerce.ui.base.ViewMessage
+import com.route.domain.models.AuthenticationResponse
 import kotlinx.coroutines.flow.StateFlow
 
 class LoginContract {
@@ -8,15 +10,16 @@ class LoginContract {
         val event : LiveData<Event>
         val state : StateFlow<State>
         fun doAction(action : Action)
-
     }
     sealed class Action{
         data object Login : Action()
     }
     sealed class Event{
-
+        data class ErrorMessage(val message : ViewMessage) : Event()
     }
     sealed class State{
-
+        data object Pending :State()
+        data object Logging : State()
+        data class Logged(val response : AuthenticationResponse) : State()
     }
 }
