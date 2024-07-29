80bdf43cabb2c579e8d089bf34b111ab3a2efb09
sherif Abdelrhman <sellkady038@gmail.com>
2024-07-29 20:25:54 +0300

    add constants keys.

--- app/src/main/java/com/example/routee_commerce/utils/Constants.kt
+++ app/src/main/java/com/example/routee_commerce/utils/Constants.kt
@@ -2,4 +2,12 @@ package com.example.routee_commerce.utils
 
 object Constants {
     const val CART_KEY="CART_KEY"
+    const val PRODUCT = "product"
+    const val PARSE_USER_DATA = "user"
+    const val USER_DATA = "authResponse"
+    const val USER_TOKEN = "token"
+    const val USER_ROLE = "role"
+    const val USER_NAME = "name"
+    const val USER_EMAIL = "email"
+    const val CART_ITEM_COUNT = "cartItemCount"
 }
