cdfe73d3453c6ade23dd1175d832382818af49ec
sherif Abdelrhman <sellkady038@gmail.com>
2024-08-01 16:35:13 +0300

    finish cart online datasource interface.

--- data/src/main/java/com/route/data/contracts/cart/CartDataSource.kt
+++ data/src/main/java/com/route/data/contracts/cart/CartDataSource.kt
@@ -1,10 +1,30 @@
 package com.route.data.contracts.cart
 
 import com.route.domain.models.Cart
+import com.route.domain.models.Products
 
 interface CartDataSource {
     suspend fun addProductToCart(
         token : String,
         productId : String
     ) : Cart<String>
+
+    suspend fun updateCartProductQuantity(
+        token : String,
+        cartProductId : String ,
+        count : String
+    ) : Cart<Products>
+
+    suspend fun getLoggedUserCart(
+        token: String
+    ) : Cart<Products>
+
+    suspend fun removeSpecificCartItem(
+        token: String ,
+        cartProductId : String
+    ) : Cart<Products>
+
+    suspend fun deleteUserCart(
+        token: String
+    ) : String
 }
