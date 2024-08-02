ae72fc9b1865cbf8f165732b64a75cca85d053f7
sherif Abdelrhman <sellkady038@gmail.com>
2024-08-02 19:48:11 +0300

    add actions in home contract add product to cart and wish list and remove product from wish list.

--- app/src/main/java/com/example/routee_commerce/ui/home/fragments/home/HomeContract.kt
+++ app/src/main/java/com/example/routee_commerce/ui/home/fragments/home/HomeContract.kt
@@ -1,7 +1,9 @@
 package com.example.routee_commerce.ui.home.fragments.home
 
 import androidx.lifecycle.LiveData
+import com.example.routee_commerce.model.WishListItem
 import com.example.routee_commerce.ui.base.ViewMessage
+import com.route.domain.models.Cart
 import com.route.domain.models.Category
 import com.route.domain.models.Products
 import kotlinx.coroutines.flow.StateFlow
@@ -17,6 +19,9 @@ class HomeContract {
 
     sealed class Action { //intent
         data class InitPage(val token: String) : Action()
+        data class AddProductToCart(val token:String,val productId : String) : Action()
+        data class AddProductToWishList(val token: String,val productId: String) : Action()
+        data class RemoveProductFromWishList(val token: String,val productId: String) : Action()
     }
 
     sealed class Event {
@@ -25,11 +30,15 @@ class HomeContract {
     }
 
     sealed class State {
+        data object Idle : State()
         data object Loading : State()
-        data class Success (val categoriesList : List<Category>? = null,
-            val mostSellingProductsList : List<Products>? = null,
-            val categoryProductsList : List<Products>?=null)
-            : State()
+        data class Success(
+            val categoriesList: List<Category>? = null,
+            val mostSellingProductsList: List<Products>? = null,
+            val categoryProductsList: List<Products>? = null,
+            val wishList: List<WishListItem>? = null,
+            val cart: List<Cart<Products>>? = null,
+        ) : State()
 
     }
 }
