3cec542e21cd63437540e54b4354467d3ad20c7b
sherif Abdelrhman <sellkady038@gmail.com>
2024-08-19 06:02:36 +0300

    add usecases in WishListViewModel.

--- app/src/main/java/com/example/routee_commerce/ui/home/fragments/wishlist/WishListViewModel.kt
+++ app/src/main/java/com/example/routee_commerce/ui/home/fragments/wishlist/WishListViewModel.kt
@@ -1,8 +1,18 @@
 package com.example.routee_commerce.ui.home.fragments.wishlist
 
 import androidx.lifecycle.ViewModel
+import com.route.domain.usecases.cart.AddProductToCardUseCase
+import com.route.domain.usecases.cart.GetLoggedUserCartUseCase
+import com.route.domain.usecases.wishlist.DeleteProductFromWishlistUseCase
+import com.route.domain.usecases.wishlist.GetLoggedUserWishlistUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class WishListViewModel : ViewModel() {
+class WishListViewModel @Inject constructor(
+    private val getLoggedUserCartUseCase: GetLoggedUserCartUseCase,
+    private val getLoggedUserWishlistUseCase: GetLoggedUserWishlistUseCase,
+    private val addProductToCartUseCase : AddProductToCardUseCase,
+    private val removeProductFromWishList: DeleteProductFromWishlistUseCase
+) : ViewModel() {
 }
